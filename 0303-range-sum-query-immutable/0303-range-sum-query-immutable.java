a20827c27f2cfa38042f651b4309c8a27c372806
Neha Geete <nehageete92@gmail.com>
2026-07-03 22:55:19 +0530

    Time: 56 ms (5.23%), Space: 47.7 MB (44.36%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -18,9 +18,9 @@ class NumArray {
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
-// public class Main{
-//     public static void main(String args[]){
-//         NumArray obj = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
-//         System.out.print(obj.sumRange(0,2));
-//     }
-// }
+public class Main{
+    public static void main(String args[]){
+        NumArray obj = new NumArray(new int[]{-2, 0, 3, -5, 2, -1});
+        System.out.print(obj.sumRange(0,2));
+    }
+}
