665923646fa99b6d50e76c22f387aa185556b321
Neha Geete <nehageete92@gmail.com>
2026-07-07 03:05:29 +0530

    Time: 0 ms (100%), Space: 43.6 MB (24.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,40 +1,22 @@
-// better
-/*class Solution {
-    public void sortColors(int[] nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0) count0++;
-            else if(nums[i] == 1) count1++;
-            else count2++;
-        }
-        for(int i=0;i<count0;i++) nums[i] = 0;
-        for(int i=count0;i<count0+count1;i++) nums[i] = 1;
-        for(int i=count0+count1;i<nums.length;i++) nums[i] = 2;
-
+class Solution {
+    private void swap(int nums[], int a , int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
         return;
     }
-}*/
-// optimal-> dutch flag
-class Solution {
     public void sortColors(int[] nums) {
-        int low=0,mid=0,high=nums.length-1;
-        while(mid<=high){
-            if(nums[mid] == 0){
-               int temp=nums[low];
-               nums[low] = nums[mid];
-               nums[mid] = temp;
-               mid++;
-               low++;
+        int low = 0 , mid = 0, high = nums.length-1;
+        while(mid <= high){
+            if(nums[mid] == 0) 
+            {
+                swap(nums, mid,low);
+                mid ++ ;
+                low ++;
             }
-            else if(nums[mid]==1){
-               mid++;
-            }
-            else{
-                int temp = nums[mid];
-                nums[mid]= nums[high];
-                nums[high] = temp;
-                high--;
-            }
+            else if (nums[mid] == 1) mid++;
+            else{swap(nums,mid,high);
+            high--;}
         }
         return;
     }
