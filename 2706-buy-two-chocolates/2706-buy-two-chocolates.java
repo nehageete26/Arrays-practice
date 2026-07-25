6ddfb2e59008dfc9fb1bf501a2da0a7502e3e808
Neha Geete <nehageete92@gmail.com>
2026-07-26 04:39:27 +0530

    Time: 1 ms (100%), Space: 46.4 MB (63.81%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,10 +1,27 @@
-class Solution {
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//         Arrays.sort(prices);
+//         int remain = 0;
+//             if(prices[0]+prices[1] == money) remain = 0;
+//             else if (prices[0]+prices[1] < money) remain = money - (prices[0]+prices[1]);
+//             else remain =  money;
+//         return remain;
+//     }
+// }
+
+class Solution{
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int remain = 0;
-            if(prices[0]+prices[1] == money) remain = 0;
-            else if (prices[0]+prices[1] < money) remain = money - (prices[0]+prices[1]);
-            else remain =  money;
-        return remain;
+        int smallest = Integer.MAX_VALUE;
+        int ssmallest = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] < smallest){
+                ssmallest = smallest;
+                smallest = prices[i];
+            }
+            else if(prices[i] < ssmallest) ssmallest = prices[i];
+        }
+        if(smallest + ssmallest <= money) 
+        return money - (smallest + ssmallest);
+        else return money;
     }
 }
