2cc3338612087cba41f652f26ea24f828e52ee56
Neha Geete <nehageete92@gmail.com>
2026-07-30 22:00:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (98.84%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+        int sum = digits[i] + carry;
+        digits[i] = sum % 10; 
+        carry = sum / 10;
+        if (carry == 0) break;
+        }
+        if (carry == 1) {
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+        }
+        return digits;
+    }
+}
