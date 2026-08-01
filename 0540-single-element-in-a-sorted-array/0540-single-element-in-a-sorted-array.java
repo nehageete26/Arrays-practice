fd7aed549b560c6adcc87765caa4298d084bf5f1
Neha Geete <nehageete92@gmail.com>
2026-08-01 14:05:40 +0530

    Time: 0 ms (100%), Space: 52.9 MB (39.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -22,33 +22,33 @@ class Solution {
     //     }
     //    return -1; 
 
-    // using binary search
-//     if(n==1) return nums[0];// edge case if only 1 element in array
-//     if(nums[0] != nums[1]) return nums[0]; // edge case for 1st element in array
-//     if(nums[n-1] != nums[n-2]) return nums[n-1]; // edge case for the last element in array
-//     int lower = 1;
-//     int upper = n-1;
-//     while(lower<=upper){
-//         int mid = (lower+upper)/2;
-//         if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
-//             return nums[mid]; // if the single element is at mid 
-//         }
-//         else if(mid%2==1 && nums[mid-1] == nums[mid] || mid%2==0 && nums[mid] == nums[mid+1])
-//         lower = mid + 1 ;
-//         else{
-//             upper = mid -1;
-//         }
+    //using binary search
+    if(n==1) return nums[0];// edge case if only 1 element in array
+    if(nums[0] != nums[1]) return nums[0]; // edge case for 1st element in array
+    if(nums[n-1] != nums[n-2]) return nums[n-1]; // edge case for the last element in array
+    int lower = 1;
+    int upper = n-1;
+    while(lower<=upper){
+        int mid = (lower+upper)/2;
+        if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
+            return nums[mid]; // if the single element is at mid 
+        }
+        else if(mid%2==1 && nums[mid-1] == nums[mid] || mid%2==0 && nums[mid] == nums[mid+1])
+        lower = mid + 1 ;
+        else{
+            upper = mid -1;
+        }
 
-//     }
-// return -1;
-//     }
-// }
-
-
-int xor = 0;
-for(int i=0;i<nums.length;i++){
-    xor^=nums[i];
-}
-return xor;
+    }
+return -1;
     }
 }
+
+
+// int xor = 0;
+// for(int i=0;i<nums.length;i++){
+//     xor^=nums[i];
+// }
+// return xor;
+//     }
+// }
