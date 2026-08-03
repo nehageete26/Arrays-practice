94d5ca5403a390b0a9ed5cce4c5762db8f454de6
Neha Geete <nehageete92@gmail.com>
2026-08-04 02:01:35 +0530

    Time: 302 ms (6.96%), Space: 47.8 MB (24.79%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int dup = 0;
-        int missing = 0;
         int ans[] = new int[2];
+        int dup = 0;
         ArrayList<Integer> list = new ArrayList<>();
         int sum = (nums.length*(nums.length+1))/2;
         for(int i=0;i<nums.length;i++){
             for(int j = i+1;j<nums.length;j++){
                 if(nums[i] == nums[j]){
-                   dup = nums[i];
+                    dup = nums[i];
                    list.add(dup);
                    break;
                 }
@@ -25,3 +24,17 @@ class Solution {
     return ans;
     } 
 }
+
+        //int[] ans=new int[2];
+        // Set<Integer> s = new HashSet<>();
+        // for(int x:nums){
+        //     if(!s.add(x))
+        //            ans[0] = x;
+        // }
+        // for(int i=1;i<=nums.length;i++) {
+        //     if(!s.contains(i)){
+        //         ans[1]=i;
+        //         break;
+        //     }
+        // }
+        // return ans;
