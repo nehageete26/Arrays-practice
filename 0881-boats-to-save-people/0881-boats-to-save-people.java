6044f8c36572022857f214a599fd85246320e9e2
Neha Geete <nehageete92@gmail.com>
2026-08-22 03:21:22 +0530

    Time: 19 ms (98.84%), Space: 56.7 MB (20.72%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0 , right = people.length-1;
+        int count = 0;
+        while(left <= right){
+            if(people[left]+people[right] <= limit){
+                count++;
+                left++;
+                right--;
+            }else{
+                count++;
+                right--;
+            }
+        }
+        return count;
+    }
+}
