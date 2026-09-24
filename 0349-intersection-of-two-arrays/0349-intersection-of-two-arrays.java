e42fc583647b692fbc182882323b50abeac167f7
Neha Geete <nehageete92@gmail.com>
2026-09-24 13:32:46 +0530

    Time: 2 ms (97.77%), Space: 45.2 MB (25.62%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -3,16 +3,35 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         HashSet<Integer> ans = new HashSet<>();
         for(int i=0;i<nums1.length;i++){
-           set.add(nums1[i]);
+            set.add(nums1[i]);
         }
         for(int i=0;i<nums2.length;i++){
             if(set.contains(nums2[i])) ans.add(nums2[i]);
         }
         ArrayList<Integer> list = new ArrayList<>(ans);
-         int[] result = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-          result[i] = list.get(i);
+        int arr[] = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            arr[i] = list.get(i);
         }
-        return result;
+        return arr;
     }
 }
+
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         HashSet<Integer> set = new HashSet<>();
+//         HashSet<Integer> ans = new HashSet<>();
+//         for(int i=0;i<nums1.length;i++){
+//            set.add(nums1[i]);
+//         }
+//         for(int i=0;i<nums2.length;i++){
+//             if(set.contains(nums2[i])) ans.add(nums2[i]);
+//         }
+//         ArrayList<Integer> list = new ArrayList<>(ans);
+//          int[] result = new int[list.size()];
+//         for(int i = 0; i < list.size(); i++){
+//           result[i] = list.get(i);
+//         }
+//         return result;
+//     }
+// }
